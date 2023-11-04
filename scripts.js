4fcfa508c0a5022fb5e63911bf0bb01d78d0799d
bombies <xjamesdzn@gmail.com>
2023-11-04 09:58:40 -0500

    Implement form handling

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
-/* Add your JavaScript to this file */
+let DOM_LOADED = false
+const queuedJobs = []
+
+window.addEventListener('load', () => {
+    DOM_LOADED = true
+
+    while (queuedJobs.length)
+        queuedJobs.pop()() // I love JavaScript
+})
+
+/**
+ * This function is used to do any DOM-related
+ * work once the DOM has loaded.
+ * @param cb Any callback function
+ */
+const work = (cb) => {
+    if (!DOM_LOADED)
+        return queuedJobs.push(cb)
+    return cb()
+}
+
+// Handle form submit
+work(() => {
+    const newsLetterForm = document.querySelector(".newsletter .container form")
+    const messageDiv = document.querySelector(".newsletter .container .message")
+
+    newsLetterForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const formData = new FormData(newsLetterForm);
+        const email = formData.get("email")
+        messageDiv.innerHTML = email ?
+            `Thank you! Your email address (${email}) has been added to our mailing list!` :
+            "Please enter a valid email address."
+    })
+})
